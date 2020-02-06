0b64e553928fe2ab55010c4b30658e70b04e8377
AlexL <agarza@ucsd.edu>
2020-02-06 10:58:18 -0800

    fixed

--- routes/add.js
+++ routes/add.js
@@ -1,11 +1,5 @@
 var data = require("../data.json");
 
 exports.addFriend = function(request, response) {
-	var first = request.query.name;
-	var second = request.query.description;
-
-	var newFriend = {
-		"name:" 
-	}
-	data.friends.push(newFriend);
+	
 }
